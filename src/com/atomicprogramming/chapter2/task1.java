f3e6d6af3968fe320cf82298d749c9e7ee45633d
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-01 22:32:24 +0000

    Finished task-1

--- src/com/atomicprogramming/chapter2/task1.java
+++ src/com/atomicprogramming/chapter2/task1.java
@@ -1,7 +1,19 @@
 package com.atomicprogramming.chapter2;
 
 public class task1 {
+
+    public static int find_perimeter(int length, int width) {
+        return (length * 2) + (width * 2);
+    }
+
+    public static int find_area(int length, int width) {
+        return length * width;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int length = 18;
+        int width = 7;
+        System.out.println("The perimeter of the rectangle is " + find_perimeter(length, width));
+        System.out.println("The area of the rectangle is " + find_area(length, width));
     }
 }
